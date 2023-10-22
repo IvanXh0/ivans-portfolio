c2f1365ec0ed73c7203020e63e48344eb51d4795
IvanXh0 <ivan.apostolovski@gmail.com>
2023-10-23 00:28:30 +0200

    header done

--- components/header.tsx
+++ components/header.tsx
@@ -5,41 +5,60 @@ import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
 import useActiveStore from "@/store/active.store";
+import clsx from "clsx";
 
 export default function Header() {
-  const {
-    activeSection,
-    setActiveSection,
-    timeOfLastClick,
-    setTimeOfLastClick,
-  } = useActiveStore();
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveStore();
 
   return (
-    <motion.header
-      className="absolute inset-x-0 top-0 z-50"
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-    >
-      <nav
-        className="flex items-center justify-center p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <motion.div
-          className="flex gap-x-3 sm:gap-x-5 md:gap-x-6 lg:gap-x-10"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-        >
-          {links.map((item) => (
-            <Link
-              key={item.name}
-              href={item.hash}
-              className="text-sm font-semibold leading-6 text-gray-900 hover:text-teal-500 hover:underline-offset-4 hover:underline"
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full sm:top-6 sm:h-[3.25rem] sm:w-[36rem]"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-semibold text-slate-900 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              {item.name}
-            </Link>
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ",
+                  {
+                    "text-violet-600 ": activeSection === link.name,
+                  },
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span
+                    className="border-b border-violet-500 absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
           ))}
-        </motion.div>
+        </ul>
       </nav>
-    </motion.header>
+    </header>
   );
 }
